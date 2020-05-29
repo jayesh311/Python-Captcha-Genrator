08c321cad481dd3d3c1ddf0a7a4ed4d13c507238
jayesh311 <61532473+jayesh311@users.noreply.github.com>
2020-05-29 23:14:04 +0530

    Add files via upload

---
+++ CaptchaInPy.py
@@ -0,0 +1,19 @@
+#Random Captcha generator in python 3.8 
+import random
+import string
+
+def Captcha(stringLength=8):
+    letters = string.ascii_letters
+    #join function join's tuple,string,list by given parameter
+    #random.choice(parameter)<--select random obj present in parameter
+    captcha = ''.join(random.choice(letters) for i in range(stringLength))
+    return captcha
+cap = Captcha()
+print(cap)
+p = input("Enter the Captcha:")
+if p == cap:
+    print("you enter right captcha")
+else:
+    print("wrong captcha")
+
+    
